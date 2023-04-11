c801d3a39b97026b0bc8238c9d1cb40b50e6d050
Chonghao Sima <simac@purdue.edu>
2023-04-11 21:02:07 +0800

    Update vis.py

--- utils/vis.py
+++ utils/vis.py
@@ -144,12 +144,12 @@ def vis_nuscene():
     vis_voxel_size = 0.4
 
     # If you want to vis the data file provided in GitHub, use the code below
-    file = "data/29796060110c4163b07f06eff4af0753/labels.npz"
+    file = "data/29796060110c4163b07f06eff4af0753/labels.npz" # change this to the file path on your machine
     data = np.load(file)
     semantics, mask_lidar, mask_camera = data['semantics'], data['mask_lidar'], data['mask_camera']
 
     # If you want to vis the gt files in mini & trainval, use the code below
-    # file_gt = 'data/gts/scene-0061/0cd661df01aa40c3bb3a773ba86f753a'
+    # file_gt = 'data/gts/scene-0061/0cd661df01aa40c3bb3a773ba86f753a' # change this to the gt folder path on your machine
     # mask_camera_file = os.path.join(file_gt, 'mask_camera.npz')
     # mask_lidar_file = os.path.join(file_gt, 'mask_lidar.npz')
     # semantics_file = os.path.join(file_gt, 'semantics.npz')
